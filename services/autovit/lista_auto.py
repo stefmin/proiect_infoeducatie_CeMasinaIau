6eb04538ec1fe929dab444b68b04c0f65323e12d
stefmin <mineateo@gmail.com>
2024-10-17 20:43:18 +0300

    Add files via upload

--- services/autovit/lista_auto.py
+++ services/autovit/lista_auto.py
@@ -7,7 +7,7 @@ def parse_pagina_auto(page_content):
     container_masina = htmlpage.css("article.ooa-yca59n")
     for item in container_masina:
         try:
-            link_masina = item.css_first("h1.e1i3khom9").css_first("a").attributes["href"]
+            link_masina = item.css_first("h1.epwfahw9").css_first("a").attributes["href"]
         except AttributeError:
             link_masina = None
 
